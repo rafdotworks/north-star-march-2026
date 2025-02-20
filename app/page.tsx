611c1c17b722542e96e6d5e180bffdb73bc7ede5
raf <rafvitale@chainml.net>
2025-02-20 11:10:15 -0500

    image -- commenting marianne for now

--- app/page.tsx
+++ app/page.tsx
@@ -33,15 +33,15 @@ export default function Page() {
   ];
 
   const photos = [
-    "/photos/marianne.jpeg",
-    "/photos/josh.JPG",
-    "/photos/omar.JPG",
-    "/photos/jordi.JPG",
-    "/photos/adrien.JPG",
-    "/photos/flo.JPG",
-    "/photos/kelindi.JPG",
-    "/photos/vin.JPG",
-    "/photos/anna.JPG",
+    // "/photos/marianne.jpeg",
+    "/photos/josh.jpg",
+    "/photos/omar.jpg",
+    "/photos/adrien.jpg",
+    "/photos/jordi.jpg",
+    "/photos/flo.jpg",
+    "/photos/kelindi.jpg",
+    "/photos/vin.jpg",
+    "/photos/anna.jpg",
   ];
 
   useEffect(() => {
