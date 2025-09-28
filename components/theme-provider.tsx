25c302c9278ab0934be8453a1b02aaed0c529929
raffaelevitale <raffaelevitalebusiness@gmail.com>
2025-09-28 20:14:50 +0100

    chore(theme): finalize token scheme, add data-theme provider, minor formatting

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -1,11 +1,21 @@
-'use client'
+"use client";
 
-import * as React from 'react'
+import * as React from "react";
 import {
   ThemeProvider as NextThemesProvider,
   type ThemeProviderProps,
-} from 'next-themes'
+} from "next-themes";
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
-  return <NextThemesProvider {...props}>{children}</NextThemesProvider>
+  return (
+    <NextThemesProvider
+      attribute="data-theme"
+      defaultTheme="system"
+      enableSystem
+      themes={["light", "dark"]}
+      {...props}
+    >
+      {children}
+    </NextThemesProvider>
+  );
 }
