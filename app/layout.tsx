26245d15c4638615ba9e9c0247ca7921ae8c2413
Raf <raffaelevitalebusiness@gmail.com>
2025-12-15 16:17:33 +0000

    viewport

--- app/layout.tsx
+++ app/layout.tsx
@@ -115,6 +115,7 @@ export const viewport: Viewport = {
   width: "device-width",
   initialScale: 1,
   maximumScale: 1,
+  viewportFit: "cover",
   themeColor: [
     { media: "(prefers-color-scheme: light)", color: "#f5f7fa" },
     { media: "(prefers-color-scheme: dark)", color: "#14171c" },
